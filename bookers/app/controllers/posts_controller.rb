6aa2eaaf39c3344ab3c493667c58fccc5dd689a2
Nagasaki <esh6gc2@gmail.com>
2018-03-29 17:10:48 +0900

    再提出です

--- bookers/app/controllers/posts_controller.rb
+++ bookers/app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ before_action :authenticate_user!
 		@post = Post.new(post_params)
 		@post.user_id = current_user.id
 		if  @post.save
-			redirect_to post_path(post.id)
+			redirect_to post_path(@post.id)
 		else
 			@posts = Post.all
 			render:index

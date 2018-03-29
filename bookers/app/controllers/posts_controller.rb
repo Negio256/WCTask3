564aa8f8ed51ce46fbe0a51cc169726712bfd720
Nagasaki <esh6gc2@gmail.com>
2018-03-29 13:36:31 +0900

    再提出

---
+++ bookers/app/controllers/posts_controller.rb
@@ -0,0 +1,65 @@
+class PostsController < ApplicationController
+
+before_action :authenticate_user!
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		if  @post.save
+			redirect_to post_path(post.id)
+		else
+			@posts = Post.all
+			render:index
+		end
+	end
+
+	def index
+		@posts = Post.all
+		@post = Post.new
+	end
+
+	def show
+		@post = Post.new
+		@postt = Post.find(params[:id])
+	end
+
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		post = Post.find(params[:id])
+		post.update(post_params)
+		if  post.user_id = current_user.id
+			redirect_to post_path(post.id)
+		else
+			@post = Post.new
+			@posts = Post.all
+			render:index
+		end
+	end
+
+	def destroy
+		@user = User.find_by(params[:id])
+		post = Post.find(params[:id])
+		if post.user_id = current_user.id
+		post.delete
+		redirect_to posts_path
+		else
+			@post = Post.new
+			@posts = Post.all
+			render:index
+		end
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :body, :user_id)
+	end
+
+end
---
+++ bookers/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+before_action :authenticate_user!
+
+	def index
+	  @user = current_user
+	  @users = User.all
+	  @post = Post.new
+	  posts = Post.all
+	  @posts = current_user.posts.all
+	end
+
+	def show
+  	  @post = Post.new
+  	  posts = Post.all
+  	  @user = User.find(params[:id])
+	  @posts = @user.posts.all
+	end
+
+	def edit
+	  @user = User.find(params[:id])
+	end
+
+	def update
+	  @user = User.find(params[:id])
+	  if @user.update(user_params)
+      redirect_to user_path(@user.id)
+  	  else
+  	  	@post = Post.new
+  	  	@posts = Post.all
+  	  	@users = User.all
+  	  	render:edit
+  	  end
+	end
+
+	private
+	def user_params
+	  params.require(:user).permit(:name, :image, :introduction)
+	end
+
+end
